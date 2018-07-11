a3630862322057dd8ee681a73c45ced90f3a89de
Will <will7989@hotmail.com>
2018-07-11 12:57:46 +0100

    Initial Commit

---
+++ favourite-movies-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  favourite-movies-app
+//
+//  Created by Will Andrews on 11/07/2018.
+//  Copyright © 2018 Will Andrews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
